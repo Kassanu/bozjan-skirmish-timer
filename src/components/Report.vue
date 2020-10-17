692c4a91ae76dc92feb86a5ce62be70fefd0cb0c
Kassanu <kassanu@gmail.com>
2020-10-17 02:38:23 -0400

    Add a timeline chart to the reports
    Easy to see the gaps in spawns and what skirmishes overlap

--- src/components/Report.vue
+++ src/components/Report.vue
@@ -1,20 +1,24 @@
 <template>
     <div>
-        <button
-                @click="saveJson">
-                Save
-            </button>
+        <button @click="saveJson">
+            Save
+        </button>
+        <div style="width: 75%">
+            <ReportChart :series="series" :key="chartVersion" />
+        </div>
         <ReportCard v-for="card in report" :key="card.id" :card="card" />
     </div>
 </template>
 
 <script>
     import ReportCard from './ReportCard'
+    import ReportChart from './ReportChart'
 
     export default {
         name: 'Report',
         components: {
-            ReportCard
+            ReportCard,
+            ReportChart
         },
         props: {
             list: {
@@ -24,6 +28,19 @@
             timings: {
                 type: Object,
                 required: true
+            }
+        },
+        data () {
+            return {
+                chartVersion: 0
+            }
+        },
+        watch: {
+            timings() {
+                // apex charts isn't playing nice with the computed series
+                // using this :key trick to force vue to rerender the component
+                // when timings are updated
+                this.chartVersion += 1
             }
         },
         computed: {
@@ -67,8 +84,8 @@
                     })
 
                     if (timings.length > 1) {
-                        card.averages.spawnToSpawn = card.averages.spawnToSpawn / (timings.length-1)
-                        card.averages.killToSpawn = card.averages.killToSpawn / (timings.length-1)
+                        card.averages.spawnToSpawn = card.averages.spawnToSpawn / (timings.length - 1)
+                        card.averages.killToSpawn = card.averages.killToSpawn / (timings.length - 1)
                         card.averages.enemyKills = card.averages.enemyKills / timings.length
                     }
 
@@ -76,12 +93,40 @@
                 })
 
                 return report
+            },
+            series() {
+                const series = []
+
+                this.list.forEach(item => {
+                    let card = {
+                        name: item.name,
+                        data: []
+                    }
+                    if (this.timings[item.id]) {
+                        let timings = this.timings[item.id]
+                            timings.forEach((time) => {
+                            if (time[0] !== null && time[1] !== null) {
+                                let timeObject = {
+                                    x: item.name,
+                                    y: [
+                                        new Date(time[0]).getTime(),
+                                        new Date(time[1]).getTime()
+                                    ]
+                                }
+                                card.data.push(timeObject)
+                            }
+                        })
+                    }
+                    series.push(card)
+                })
+
+                return series
             }
         },
         methods: {
             saveJson() {
                 let a = document.createElement("a");
-                let file = new Blob([JSON.stringify(this.report, null, 4)], {type: 'application/json'});
+                let file = new Blob([JSON.stringify(this.report, null, 4)], { type: 'application/json' });
                 a.href = URL.createObjectURL(file);
                 a.download = `report.json`;
                 a.click();
---
+++ src/components/ReportChart.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <VueApexCharts type="rangeBar" :options="chartOptions" :series="series" />
+    </div>
+</template>
+
+<script>
+    import VueApexCharts from 'vue-apexcharts'
+
+    export default {
+        components: { VueApexCharts },
+        props: {
+            series: {
+                type: Array,
+                required: true
+            }
+        },
+        data() {
+            return {
+                chartOptions: {
+                    chart: {
+                        type: 'rangeBar'
+                    },
+                    plotOptions: {
+                        bar: {
+                            horizontal: true,
+                            barHeight: '100%'
+                        }
+                    },
+                    xaxis: {
+                        type: 'datetime'
+                    },
+                    stroke: {
+                        width: 1
+                    },
+                    fill: {
+                        type: 'solid',
+                        opacity: 0.6
+                    },
+                    legend: {
+                        position: 'top',
+                        horizontalAlign: 'left'
+                    },
+                    tooltip: {
+                        x: {
+                            format: 'HH:mm:ss',
+                            formatter: undefined,
+                        },
+                        y: {
+                            format: 'HH:mm:ss',
+                            formatter: undefined,
+                        }
+                    }
+                }
+            }
+        }
+    }
+</script>
