0345f104c04d1fa9dedb2d8550c146d522c33071
Kassanu <kassanu@gmail.com>
2020-10-18 09:00:16 -0400

    Add a button to only show CE related stuff

--- src/components/Report.vue
+++ src/components/Report.vue
@@ -52,47 +52,49 @@
                 Object.keys(this.timings).forEach(timingKey => {
                     let timings = this.timings[timingKey]
                     let item = this.list.find(item => item.id == timingKey)
-                    let card = {
-                        id: item.id,
-                        name: item.name,
-                        averages: {
-                            spawnToSpawn: 0,
-                            killToSpawn: 0,
-                            enemyKills: 0
-                        },
-                        timings: []
-                    }
-
-                    timings.forEach((time, index) => {
-                        let timeObject = {
-                            spawn: time[0],
-                            kill: time[1],
-                            sinceLastSpawn: null,
-                            sinceLastKill: null,
-                            enemyKills: time[2] || 0
+                    if (item) {
+                        let card = {
+                            id: item.id,
+                            name: item.name,
+                            averages: {
+                                spawnToSpawn: 0,
+                                killToSpawn: 0,
+                                enemyKills: 0
+                            },
+                            timings: []
                         }
-                        card.averages.enemyKills += timeObject.enemyKills
-                        if (index > 0) {
-                            let lastTime = card.timings[card.timings.length - 1]
-                            if (timeObject.spawn && lastTime.spawn) {
-                                timeObject.sinceLastSpawn = (timeObject.spawn - lastTime.spawn)
-                                card.averages.spawnToSpawn += timeObject.sinceLastSpawn
+
+                        timings.forEach((time, index) => {
+                            let timeObject = {
+                                spawn: time[0],
+                                kill: time[1],
+                                sinceLastSpawn: null,
+                                sinceLastKill: null,
+                                enemyKills: time[2] || 0
                             }
-                            if (timeObject.spawn && lastTime.kill) {
-                                timeObject.sinceLastKill = (timeObject.spawn - lastTime.kill)
-                                card.averages.killToSpawn += timeObject.sinceLastKill
+                            card.averages.enemyKills += timeObject.enemyKills
+                            if (index > 0) {
+                                let lastTime = card.timings[card.timings.length - 1]
+                                if (timeObject.spawn && lastTime.spawn) {
+                                    timeObject.sinceLastSpawn = (timeObject.spawn - lastTime.spawn)
+                                    card.averages.spawnToSpawn += timeObject.sinceLastSpawn
+                                }
+                                if (timeObject.spawn && lastTime.kill) {
+                                    timeObject.sinceLastKill = (timeObject.spawn - lastTime.kill)
+                                    card.averages.killToSpawn += timeObject.sinceLastKill
+                                }
                             }
+                            card.timings.push(timeObject)
+                        })
+
+                        if (timings.length > 1) {
+                            card.averages.spawnToSpawn = card.averages.spawnToSpawn / (timings.length - 1)
+                            card.averages.killToSpawn = card.averages.killToSpawn / (timings.length - 1)
+                            card.averages.enemyKills = card.averages.enemyKills / timings.length
                         }
-                        card.timings.push(timeObject)
-                    })
 
-                    if (timings.length > 1) {
-                        card.averages.spawnToSpawn = card.averages.spawnToSpawn / (timings.length - 1)
-                        card.averages.killToSpawn = card.averages.killToSpawn / (timings.length - 1)
-                        card.averages.enemyKills = card.averages.enemyKills / timings.length
+                        report.push(card)
                     }
-
-                    report.push(card)
                 })
 
                 return report
--- src/components/ViewReport.vue
+++ src/components/ViewReport.vue
@@ -1,5 +1,6 @@
 <template>
     <div style="width: 100%">
+        <button @click="onlyCEChains = !onlyCEChains">Show Only CE</button>
         <input type="file" accept=".json" @change="upload">
         <Report :list="fullList" :timings="timings" />
     </div>
@@ -15,6 +16,7 @@
         },
         data() {
             return {
+                onlyCEChains: false,
                 skirmishes: [
                     [
                         {
@@ -222,8 +224,45 @@
             }
         },
         computed: {
+            filteredList() {
+                if (!this.onlyCEChains) {
+                    return this.skirmishes
+                }
+                let filtered = []
+                const ceIds = [
+                    "074e37ef-e316-4e13-9d54-e2560248ef9b",
+                    "04dff65f-3716-43d1-91e6-b6545ad9202f",
+                    "701da551-51b4-4447-9be1-1990af101774",
+                    "2d094f75-dbf5-493f-a96b-01d956be2ab1",
+                    "6f856f96-d516-4932-a36d-7fe06070949a",
+                    "dbb14450-772c-46f9-b712-b9018fcb42a2",
+                    "28c7ca10-9645-4ea1-b162-9d8399f2f6a0",
+                    "0f03feb4-71fd-4718-b4d7-5883e9208d6a",
+                    "35a3df50-4854-4d2b-bd1a-c050096ea6c4",
+                    "e94f8624-5302-47d9-b40a-3593f99aff66",
+                    "1816511b-3ae7-400d-ac09-95f4feb97955",
+                    "ac7323df-5a1c-40ec-aba0-48ad25a27976",
+                    "8dc3290d-686f-411e-b8de-95eb0b3f8c1b",
+                    "ae038167-bcf2-42ec-a435-8c061e78b6f7",
+                    "a8d79769-58d9-47da-be03-71d968875128",
+                    "b5a003a3-2bd8-4fb0-9255-9ebedb0f2a31",
+                    "ae3b7dd2-84ef-4d88-8895-b7b51284529d",
+                    "35ef6853-666d-4e89-9798-9baa20a0185c",
+                    "f45e0dd1-3942-4f9a-9b11-4460baf37571",
+                    "83f30088-e0e1-409f-a69a-df20cbfac568",
+                    "a9cfa0a6-9f6b-4dac-bf82-6e4bc30e556b",
+                    "350eace7-998d-424b-814a-0ade659616c3",
+                    "585de225-67bf-4dad-bf47-f67b9c3c1fa3"
+                ]
+                this.skirmishes.forEach(skirmishes => {
+                    filtered.push(skirmishes.filter(skirmish => {
+                        return ceIds.includes(skirmish.id)
+                    }))
+                });
+                return filtered
+            },
             fullList() {
-                return this.skirmishes[0].concat(this.skirmishes[1]).concat(this.skirmishes[2]).concat(this.skirmishes[3])
+                return this.filteredList[0].concat(this.filteredList[1]).concat(this.filteredList[2]).concat(this.filteredList[3])
             }
         },
         methods: {
